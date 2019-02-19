50f8e411fccfc39baa7010a279a4e933aa556b00
austintauer <35610398+austintauer@users.noreply.github.com>
2019-02-19 11:51:25 -0500

    Added Posts to Mockup

--- Parch/Parch/src/app/component/board/board.component.ts
+++ Parch/Parch/src/app/component/board/board.component.ts
@@ -5,11 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css']
 })
+
+
 export class BoardComponent implements OnInit {
 
-  constructor() { }
+  private posts : string[][];
+  text : Array<string>  = [];
+
+  constructor() { 
+    this.posts = [["Idiot0", "post0"], ["Idiot1", "post1"], ["Idiot2", "post2"],
+     ["Idiot3", "post3"]];
+    
+    for (let i : number = 0; i < this.posts.length; i++) {
+      this.text.push(this.posts[i][0] + ": " + this.posts[i][1]);
+    }
+
+  }
 
   ngOnInit() {
+
   }
 
 }
