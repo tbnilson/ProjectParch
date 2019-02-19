4c641431ab65798f9d3ca798d7eb3441bbf9fea6
racollazo <aaroncollazo@outlook.com>
2019-02-19 10:56:46 -0500

    Hibernate Configuration file added

--- Project2/src/main/java/serf/RequestHelper.java
+++ Project2/src/main/java/serf/RequestHelper.java
@@ -1,5 +1,15 @@
 package serf;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class RequestHelper {
 
+	
+	
+	public static void Process(HttpServletRequest request, HttpServletResponse response) {
+	
+	}
+	
+	
 }
