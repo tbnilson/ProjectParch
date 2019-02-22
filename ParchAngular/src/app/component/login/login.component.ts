ef1cfcb650cc08b5f96ed8abebeccecc65076624
ChrisPrelaz <cgprelaz@gmail.com>
2019-02-22 17:49:11 -0500

    added login component with placeholder code for checking credentials adn proper registration. Included a User model file. Updated some stylings

--- ParchAngular/src/app/component/login/login.component.ts
+++ ParchAngular/src/app/component/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { defineBase } from '@angular/core/src/render3';
+import { checkAndUpdateElementInline } from '@angular/core/src/view/element';
+import { User } from 'src/app/models/User'
+import { checkBinding } from '@angular/core/src/view/util';
+//import { userInfo } from 'os';
 
 
 @Component({
@@ -16,24 +21,35 @@ export class LoginComponent implements OnInit {
   username:string;
   password:string;
   email:string;
-  testusername:string="admin";
-  testpassword:string="admin";
-  User={
-    username:this.username,
-    password:this.password,
-    email:this.password
-    }
+
  answer:string;
+  //User("admin@admin.com","admin","admin",)
+ db: Array<User>=[(new User("admin","admin","admin"))];
+ checkDB(uid){
+   if(this.username==uid.username&&this.password==uid.password){
+     this.answer="it works";
+     return uid.username;
+   }
+   else
+    return null;
+ };
+ x:User;
 Login(){
-if(this.username==this.testusername&&this.password==this.testpassword){
-  this.answer="it works";
-}
-else
-this.answer="incorrect";
-}
-// Registration(){
-//   this.User.username=this.newusername;
-// this.User.password=this.newpassword;
-//}
+  for(let i=0;i<this.db.length;i++){
+    this.checkDB(this.db[i]);
+  }
+//this.db.forEach(this.checkDB(User));
+} ;
+ newemail:string;
+ newusername:string;
+ newpassword:string; 
+
+check:string;
+Register(){
+  this.db.push(new User(this.newemail,this.newusername,this.newpassword,));
+  this.check="registered"
+  console.log(this.db);
+}; 
 
 }
+
