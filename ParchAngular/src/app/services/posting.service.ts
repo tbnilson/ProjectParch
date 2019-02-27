4a438cda41faff114656c25323041f62073489a0
tbnilson <tbnilson@gmail.com>
2019-02-27 13:15:06 -0500

    Update posting.service.ts

--- ParchAngular/src/app/services/posting.service.ts
+++ ParchAngular/src/app/services/posting.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostingService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/x-www-form-urlencoded',
+      'X-Requested-With': 'XMLHttpRequest'
+    })
+  };
+
 }
