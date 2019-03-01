9e21a194118e23267e17aebd3bc5a32146cfd6e0
ChrisPrelaz <cgprelaz@gmail.com>
2019-03-01 10:18:57 -0500

    unique ids added

--- ParchAngular/Protractor/parch.js
+++ ParchAngular/Protractor/parch.js
@@ -8,21 +8,21 @@ describe("Parch Login and Registration test suite",function(){
         browser.get("http://ec2-18-207-247-77.compute-1.amazonaws.com:4200/login")
         expect(browser.getTitle()).toBe("Parch")
     });
-    it("Should register a new user",function(){
+    /* it("Should register a new user",function(){
         element(by.name("Email")).sendKeys("email@domain.com")
         element(by.name("NewUsername")).sendKeys("flaming")
         element(by.name("NewPassword")).sendKeys("elmo")
         magnifiyingglass = element(by.id("reg"))
         magnifiyingglass.click()
         expect(element(by.id("parchSnackbarElem"))).toBe("You successfully registered your account!")
-    });
-    it("Should login as the newly created user",function(){
-        element(by.name("Username")).sendKeys("flaming")
-        element(by.name("Password")).sendKeys("elmo")
+    }); */
+    it("Should login as my test account",function(){
+        element(by.name("Username")).sendKeys("ChrisPrime")
+        element(by.name("Password")).sendKeys("Prime1")
         magnifiyingglass = element(by.id("log"))
         magnifiyingglass.click()
         browser.sleep(2000)
-        expect(element(by.id("CurrentUsernameDisplay"))).toBe("flaming")
+        expect(element(by.id("CurrentUsernameDisplay"))).toBe("ChrisPrime")
     });
     it("Should logout of Parch",function(){
         element(by.name("logout"))
@@ -32,15 +32,15 @@ describe("Parch Login and Registration test suite",function(){
     });
     it("Should fail to register",function(){
         element(by.name("Email")).sendKeys("email@domain.com")
-        element(by.name("NewUsername")).sendKeys("flaming")
-        element(by.name("NewPassword")).sendKeys("elmo")
+        element(by.name("NewUsername")).sendKeys("ChrisPrime")
+        element(by.name("NewPassword")).sendKeys("Prime1")
         magnifiyingglass = element(by.id("reg"))
         magnifiyingglass.click()
         expect(element(by.id("parchSnackbarElem"))).toBe("Something went wrong with registration") 
     })
     it("Should fail to login",function(){
-        element(by.name("Username")).sendKeys("flaming")
-        element(by.name("Password")).sendKeys("grover")
+        element(by.name("Username")).sendKeys("ChrisPrime")
+        element(by.name("Password")).sendKeys("Prime10")
         magnifiyingglass = element(by.id("log"))
         magnifiyingglass.click()
         expect(element(by.id("parchSnackbarElem"))).toBe("That was not a valid login") 
