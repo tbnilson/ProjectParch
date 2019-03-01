78c3f45428eab08566ff9004ba8c3d45fa739c51
austintauer <35610398+austintauer@users.noreply.github.com>
2019-03-01 14:35:10 -0500

    Revert "Merge pull request #119 from tbnilson/angular"
    This reverts commit de5118bff7bef752e4775cc2fa84f234dea1e52d, reversing
    changes made to 05e0a7aedc81d4c94f36b6d3da3eca6124c6150c.

--- ParchAngular/src/app/models/User.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    username:string;
-    email:string;
-    
-    constructor(em:string,unme:string) {
-      this.username=unme;
-      this.email=em;
-    }
-  }
--- ParchAngular/src/app/pipes/sanitize-str.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SanitizeStrPipe } from './sanitize-str.pipe';
-
-describe('SanitizeStrPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SanitizeStrPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
